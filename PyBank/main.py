6ac43fcfc771dc6f330895209876d83dcede3db4
Your Name <you@example.com>
2020-11-09 20:17:12 -0600

    Added Avg Change as well as Greatest Inc and Dec.

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,30 +9,51 @@ with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter= ',')
     next(csvreader, None) #Iterating Over Header
 
-### Counting the Number of Months
-    totalMonths = 0
 ### Counting the Net Profit/Losses
     netProfit = 0
+### Date Array
+    Data = []
+### Profit Array
+    Profit = []
 
 ### Looping through Dataset
     for row in csvreader:
-        totalMonths += 1 ### Calculating total number of months
-        netProfit += int(row[1]) ### Calculating net profit/loss
-        ### Finding the Last Value of Dataset
-        lastProfit = row[1]
+        Data.append(row[0])
+        Profit.append(row[1])
 
-    
+### Calculating total number of months
+totalMonths = len(Data)
 
+### Calculating net profit/loss (Condense Loop)
+for row in Profit:
+    netProfit += int(row)
 
-    
+### Calculating Greatest Profit Increase/Decrease
+greatInc = 0
+greatDec = 0
+for i in range(len(Profit)- 1): #think about later
+    initial = Profit[i-1]
+    final = Profit[i]
+    change = int(final) - int(initial)
 
-        
+    if change > greatInc:
+        greatInc = change
+        IncDate = Data[i]
+    elif change < greatDec:
+        greatDec = change
+        DecDate = Data[i]
 
-#profitChange = lastProfit -
-    print('Financial Analysis')
-    print('----------------------------')
-    print(f'Total Months: {totalMonths}')
-    print(f'Total: ${netProfit}')
-    print(lastProfit)
-    #print(firstProfit)
+### Calculating Average Change
+finalProfit = int(Profit[totalMonths - 1])
+firstProfit = int(Profit[0])
+periodChange = finalProfit - firstProfit
+AvgChange = round(periodChange / (totalMonths - 1),2)
+
+print('Financial Analysis')
+print('----------------------------')
+print(f'Total Months: {totalMonths}')
+print(f'Total: ${netProfit}')
+print(f'Average Change: {AvgChange}')
+print(f'Greatest Increase in Profits: {IncDate} ({greatInc})')
+print(f'Greatest Decrease in Profits: {DecDate} ({greatDec})')
 
